7dbe28acafbe6fc938c7f64a8ad142f32dbf962f
James <53650971+jmstudyacc@users.noreply.github.com>
2022-03-08 23:15:16 +0000

    testing out hyper

--- sdwan/src/main.rs
+++ sdwan/src/main.rs
@@ -1,77 +1,49 @@
-extern crate reqwest;
+#![deny(warnings)]
+#![warn(rust_2018_idioms)]
+use std::env;
 
-use sdwan::{vmanage_get, vmanage_login};
-//use reqwest::StatusCode;
+use hyper::{body::HttpBody as _, Client};
+use hyper_tls::HttpsConnector;
+use tokio::io::{self, AsyncWriteExt as _};
+
+// A simple type alias so as to DRY.
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
-async fn main() {
-    vmanage_login(
-        "https://sandbox-sdwan-1.cisco.com",
-        "devnetuser",
-        "RG!_Yw919_83",
-    )
-    .await;
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
 
-    let res = vmanage_get(
-        "https://sandbox-sdwan-1.cisco.com",
-        "/dataservice/client/token",
-    )
-    .await;
-    println!("{:?}", res.);
-    // println!();
-    // // bind a variable to hold the sdwan url
-    // let endpoint_url = "https://sandbox-sdwan-1.cisco.com";
-    // let login_action = "/j_security_check";
-    // let login_url = endpoint_url.to_owned() + login_action;
-    // let user = "devnetuser";
-    // let password = "RG!_Yw919_83";
+    // Some simple CLI args requirements...
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Usage: client <url>");
+            return Ok(());
+        }
+    };
 
-    // match res.error_for_status() {
-    //     Ok(_res) => println!("A Great Success!"),
-    //     Err(res) => println!("{:?}", res.status()),
-    // };
-    //println!("A Great success!");
-    //println!("Response text: {:?}", res.text().await?)
-    //}
+    let url = url.parse::<hyper::Uri>().unwrap();
 
-    /*
-    Using the client:
-        1. Form a GET request
-        2. SEND the request
-        3. send() returns a FUTURE so we need to .await? it
-    */
-    //let result_debug = client.get(endpoint_url).send().await?;
-
-    /*
-    As above but with the following extra steps:
-        4. text() returns the response in TEXT form
-        5. a final await to return the future and the GET request in text form
-    Once you do this the returned info is in text and you have lost a lot of manipulation
-    */
-    // let result_text = client.get(endpoint_url).send().await?.text().await?;
+    fetch_url(url).await
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn fetch_url(url: hyper::Uri) -> Result<()> {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
-    /*
-    async fn compare_client_get() : test to compare the output of using
-    the reqwest::client vs. not using the client but using the same
-    builder patterns
-    */
-    #[tokio::test]
-    async fn compare_client_get() -> Result<(), reqwest::Error> {
-        let endpoint_url = "https://sandbox-sdwan-1.cisco.com";
+    let mut res = client.get(url).await?;
 
-        // if you don't plan to make multiple requests the below can be used
-        let body = reqwest::get(endpoint_url).await?.text().await?;
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
 
-        // creating a new reqwest client
-        let client = reqwest::Client::new();
-        let result = client.get(endpoint_url).send().await?.text().await?;
-
-        // using a client or inline returns the same when using the same builder pattern
-        Ok(assert_eq!(body, result))
+    // Stream the body, writing each chunk to stdout as we get it
+    // (instead of buffering and printing at the end).
+    while let Some(next) = res.data().await {
+        let chunk = next?;
+        io::stdout().write_all(&chunk).await?;
     }
+
+    println!("\n\nDone!");
+
+    Ok(())
 }
